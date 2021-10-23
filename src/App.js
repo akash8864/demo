b6665cc489530a96d18397d32d07a532972ac65b
akash yadav <akashyadav0112@gmail.com>
2021-10-23 12:05:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => (
+    <BrowserRouter>
+        <h1>hello akash</h1>
+    </BrowserRouter>
+);
+
+export default App;
